e16f3a1e8df5e5bb941fb854df2ac4e812ade4e7
Lucas Marchi <lucas-marchi@hotmail.com>
2018-01-20 21:28:06 -0200

    Finalizando  parte 1 do curso

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -10,10 +10,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FotoModule } from './foto/foto.module';
 import { PainelModule } from './painel/painel.module';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [ BrowserModule, FotoModule, PainelModule, HttpModule, FormsModule, routing  ],
+    imports: [ BrowserModule, FotoModule, PainelModule, HttpModule, FormsModule, routing, ReactiveFormsModule  ],
     declarations: [ AppComponent, CadastroComponent, ListagemComponent ],
     bootstrap: [ AppComponent ]
 })
